013532a3ac890d988e9cf1972f973227618992f2
NONPLAYT <76615486+NONPLAYT@users.noreply.github.com>
2025-03-05 21:47:35 +0300

    update PlayerAdvancements with synchronized map + remove ConcurrentReferenceHashMap

--- divinemc-server/src/main/java/org/bxteam/divinemc/util/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package org.bxteam.divinemc.util;
-
-import org.jetbrains.annotations.Nullable;
-
-public final class StringUtil {
-    public static boolean hasLength(@Nullable String str) {
-        return (str != null && !str.isEmpty());
-    }
-}
